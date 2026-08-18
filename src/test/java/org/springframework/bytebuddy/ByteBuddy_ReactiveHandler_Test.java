8882e8af3a81e4477f7c14412256553ada362ae0
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-18 10:58:22 +0800

    refactor: groupId io.github.hiwepy -> io.github.easy4j, package rename

--- src/test/java/org/springframework/bytebuddy/ByteBuddy_ReactiveHandler_Test.java
+++ src/test/java/org/springframework/bytebuddy/ByteBuddy_ReactiveHandler_Test.java
@@ -27,7 +27,7 @@ import net.bytebuddy.matcher.ElementMatchers;
 /**
  * http://bytebuddy.net/#/tutorial
  * http://xiangshouxiyang.iteye.com/blog/2377664
- * @author <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class ByteBuddy_ReactiveHandler_Test {
 
